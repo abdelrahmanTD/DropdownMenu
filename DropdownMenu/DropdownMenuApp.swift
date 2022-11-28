e57ecf8b8188642d181c03c74d5294eb6313949a
abdelrahmanTalaat <abdelrahman_adm@outlook.com>
2022-11-28 21:45:11 +0200

    Initial Commit

---
+++ DropdownMenu/DropdownMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropdownMenuApp.swift
+//  DropdownMenu
+//
+//  Created by Abdelrahman Talaat on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DropdownMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
